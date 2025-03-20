18239e436dc1dbdd7ea3e516e95c1eeed7b89d8b
minjeong <01minjung@gmail.com>
2025-03-20 17:26:05 +0900

    상품등록

--- src/features/goods/api/httpService.js
+++ src/features/goods/api/httpService.js
@@ -2,46 +2,41 @@ import axios from "axios";
 
 
 // 상품 저장 (파일 포함)
-export const saveGoods = async (formData) => {
-  console.log("formData1:", formData)
-  try {
-      const response = await axios.post("http://localhost:8090/app/goods/save", 
-          formData, {
-        headers: {
-          "Content-Type":"multipart/form-data"
-          
-        }
-      });
-     return response.data;
+// export const saveGoods = async (formData) => {
+//   console.log("formData1:", formData)
+//   try {
+//       const response = await axios.post("http://localhost:8090/app/goods/save", 
+//           formData, {
+//             headers: { "Content-Type": "multipart/form-data" }
+//       });
+//      return response.data;
 
-  } catch (error) {
-      console.error("상품 등록 중 에러 발생:", error);
-      throw error;
-  }
-};
+//   } catch (error) {
+//       console.error("상품 등록 중 에러 발생:", error);
+//       throw error;
+//   }
+// };
 
 export async function fetchFileUpload(formData) {
-  const response = await axios.post("http://localhost:8090/app/goods/save" , formData,
-      {
+  console.log("📌 FormData 확인:", formData);
+
+  try {
+      const response = await axios.post("http://localhost:8090/app/goods/save", formData, {
           headers: {
-            "Content-Type": "multipart/form-data",
+              "Content-Type": "multipart/form-data"
           }
+      });
+
+      console.log("✅ 서버 응답:", response.data);
+      return response.data;
+  } catch (error) {
+      console.error("🚨 상품 등록 중 에러 발생:", error);
+      if (error.response && error.response.data) {
+          console.log("📌 서버 응답:", error.response.data);
       }
-  );  // user는  JSON 형식임
-
-    if (!response === 200) {  // 무조건 200 이 아님. 서버에서 응답하는 statsu 확인 필요.
-           throw new Error('Failed to insert user data.');
-    }
-    
-      
-    var  resData = await response.data;
-         console.log("resData:" , resData)
-    return resData;
+      throw error;
+  }
 }
-
-
-
-
 
 
 // 1. 전체 상품 목록 가져오기
