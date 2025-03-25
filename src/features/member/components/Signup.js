23d1dc44ca1587118ca071321e13512bbd4f2965
Hyemin <star970909@gmail.com>
2025-03-25 21:36:14 +0900

    로그인 화면 UI

--- src/features/member/components/Signup.js
+++ src/features/member/components/Signup.js
@@ -1,4 +1,5 @@
 // 회원가입 컴포넌트
+import { Button, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -25,7 +26,7 @@ const Signup = () => {
 
     if (response.ok) {
       alert("회원가입 성공!");
-      navigate("/app/member/login"); // 회원가입 후 로그인 페이지로 이동
+      navigate("/login"); // 회원가입 후 로그인 페이지로 이동
     } else {
       alert("이미 존재하는 아이디입니다.");
     }
@@ -33,23 +34,49 @@ const Signup = () => {
 
   return (
     <div>
-      <h2>회원가입</h2>
-      <form onSubmit={handleSignup}>
-        <input
-          type="text"
-          placeholder="아이디"
-          value={memberId}
-          onChange={(e) => setMemberId(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="비밀번호"
-          value={memberPasswd}
-          onChange={(e) => setMemberPasswd(e.target.value)}
-          required
-        />
-        <button type="submit">회원가입</button>
+      <form
+        onSubmit={handleSignup}
+        className="flex flex-col justify-center items-center h-full space-y-4"
+      >
+        <div className="flex items-center space-x-4">
+          <label
+            htmlFor="memberId"
+            className="text-lg text-gray-700 dark:text-gray-300 w-24"
+          >
+            아이디
+          </label>
+          <TextInput
+            type="text"
+            placeholder="아이디"
+            value={memberId}
+            onChange={(e) => setMemberId(e.target.value)}
+            required
+            className="p-2 border rounded-md"
+          />
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <label
+            htmlFor="memberPasswd"
+            className="text-lg text-gray-700 dark:text-gray-300 w-24"
+          >
+            비밀번호
+          </label>
+          <TextInput
+            type="password"
+            placeholder="비밀번호"
+            value={memberPasswd}
+            onChange={(e) => setMemberPasswd(e.target.value)}
+            required
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <Button
+          type="submit"
+          className="w-full py-2 mt-4 text-2xl bg-blue-400 text-white rounded-md hover:bg-blue-500"
+        >
+          회원가입
+        </Button>
       </form>
     </div>
   );
