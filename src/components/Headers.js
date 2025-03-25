d1b9ab6eca5e8a95d281c66016ee9306b1e2d81d
Hyemin <star970909@gmail.com>
2025-03-25 14:09:16 +0900

    로그인, 회원가입(유정)

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -1,8 +1,11 @@
 import Time from "./Time";
 import Weather from "./Weather";
 import { Link } from "react-router-dom";
+import { useUser } from "../features/member/UserContext";
 
 export default function Headers() {
+  const { user, setUser } = useUser();
+  // console.log("현재 user 값:", user);
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
@@ -18,15 +21,33 @@ export default function Headers() {
             </span>
           </a>
           <div className="flex items-center lg:order-2">
-            <Link
-              to="app/member/login"
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            >
-              로그인
-            </Link>
-            <div className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
-              본점
-            </div>
+            {!user ? (
+              <>
+                <Link
+                  to="/app/member/signup"
+                  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                >
+                  회원가입
+                </Link>
+                <Link
+                  to="/app/member/login"
+                  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                >
+                  로그인
+                </Link>
+              </>
+            ) : (
+              <>
+                <div>{user.memberId}</div>
+
+                <button
+                  onClick={() => setUser(null)}
+                  style={{ marginLeft: "10px" }}
+                >
+                  로그아웃
+                </button>
+              </>
+            )}
             {/* 날씨 + 날짜/시간 넣을 곳 */}
             <Time />
             <Weather />
