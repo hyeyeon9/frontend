29cfd07fc8efbfe8222e97558e5dfb16aacf6115
hyeyeon9 <guhaa03@daum.net>
2025-04-03 17:30:26 +0900

    연관상품 구현하기

--- frontend/src/features/shop/api/HttpShopService.js
+++ frontend/src/features/shop/api/HttpShopService.js
@@ -83,3 +83,29 @@ export async function fetchGetTop10Items() {
 
   return response.data;
 }
+
+// goods_id로 sub_name 가져오기
+export async function fetchSubName(goodsId) {
+  const response = await 
+  axios.get(`http://localhost:8090/app/goods/subName?goodsId=${goodsId}`);
+
+  if (response.status !== 200) {
+    console.log("예외발생");
+    throw new Error("fetchSubName 예외발생");
+  }
+
+  return response.data;
+}
+
+// 연관상품 가져오기
+export async function fetchRecommendations(subName) {
+  const response = await 
+  axios.get(`http://localhost:8090/app/goods/recommendations?subName=${subName}`);
+
+  if (response.status !== 200) {
+    console.log("예외발생");
+    throw new Error("fetchRecommendations 예외발생");
+  }
+
+  return response.data;
+}
